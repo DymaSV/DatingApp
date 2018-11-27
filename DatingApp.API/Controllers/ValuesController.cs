13d0b1ad69f4fa9ab4dcd20519f89786614db883
Dmytro Sv <--global>
2018-11-27 08:00:07 +0200

    Added Authorize attribute to ValuesController. Added Authorization to Startup class (middleware).

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -26,11 +28,12 @@ namespace DatingApp.API.Controllers
             return Ok(values);
         }
 
+        [AllowAnonymous]
         // GET api/values/5
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
-            return Ok(await _context.Values.Where(w=>w.Id == id).ToListAsync());
+            return Ok(await _context.Values.Where(w => w.Id == id).ToListAsync());
         }
 
         // POST api/values
