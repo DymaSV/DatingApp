e2ca7bf46ede13c5f6735dbe16909c929d7771f4
Dmytro Sv <--global>
2019-01-27 21:35:13 +0200

    Added alertifyjs package

--- DatingApp-SPA/src/app/services/errorInterceptor.ts
+++ DatingApp-SPA/src/app/services/errorInterceptor.ts
@@ -1,36 +1,47 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler, HttpErrorResponse, HttpResponseBase, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpRequest,
+  HttpHandler,
+  HttpErrorResponse,
+  HttpResponseBase,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
-      catchError(
-        error => {
-          if (error instanceof HttpErrorResponse) {
-            if (error.status === 401) {
-              return throwError(error.statusText);
-            }
-            const applicationError = error.headers.get('Application-Error');
-            if (applicationError) {
-              console.error(applicationError);
-              return throwError(applicationError);
-            }
-            const serverError = error.error;
-            let modalStateError = '';
-            if (serverError && typeof serverError === 'object') {
-              for (const key in serverError) {
-                if (serverError[key]) {
-                  modalStateError += serverError[key] + '\n';
-                }
+      catchError(error => {
+        if (error instanceof HttpErrorResponse) {
+          if (error.status === 401) {
+            return throwError(error.statusText);
+          }
+          const applicationError = error.headers.get('Application-Error');
+          if (applicationError) {
+            console.error(applicationError);
+            return throwError(applicationError);
+          }
+          const serverError = error.error;
+          let modalStateError = '';
+          if (serverError && typeof serverError === 'object') {
+            for (const key in serverError) {
+              if (serverError[key]) {
+                modalStateError += serverError[key] + '\n';
               }
             }
-            return throwError(applicationError || modalStateError || 'Server Error');
           }
+          return throwError(
+            applicationError || modalStateError || 'Server Error'
+          );
         }
-      )
+      })
     );
   }
 }
